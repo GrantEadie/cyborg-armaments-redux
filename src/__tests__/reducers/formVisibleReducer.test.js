aa63bcefd329625ea94f55d8bd811650aea71de0
Grant Eadie <grantleadie@gmail.com>
2020-12-04 15:06:00 -0800

    finalize testing.

---
+++ src/__tests__/reducers/formVisibleReducer.test.js
@@ -0,0 +1,15 @@
+import formVisibleOnPageReducer from '../../reducers/form-visible-reducer'
+
+describe('formVisibleOnPageReducer', () => {
+  
+  test('Should return default state if no action is triggered', () => {
+    expect(formVisibleOnPageReducer(false, { type: null })).toEqual(false);
+  })
+
+  test('should return the opposite state when passed in action', () => {
+    const action = {
+      type: 'FORM_VISIBLE',
+    }
+    expect(formVisibleOnPageReducer(false, action)).toEqual(true);
+  })
+})
