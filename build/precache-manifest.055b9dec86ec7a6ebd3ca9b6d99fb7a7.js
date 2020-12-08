923470119c5120bbad98adc600d7759c440efd73
Grant Eadie <grantleadie@gmail.com>
2020-12-08 10:42:50 -0800

    add build

---
+++ build/precache-manifest.055b9dec86ec7a6ebd3ca9b6d99fb7a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f9d648fc8b7c6023543567c9e99e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8704bae43e7cdb07c14b",
+    "url": "/static/css/main.4b66db09.chunk.css"
+  },
+  {
+    "revision": "94da738ef33bfadfe47a",
+    "url": "/static/js/2.4400744b.chunk.js"
+  },
+  {
+    "revision": "4b7b99452c933bd2bd01",
+    "url": "/static/js/3.d6f11cac.chunk.js"
+  },
+  {
+    "revision": "8704bae43e7cdb07c14b",
+    "url": "/static/js/main.318cdb44.chunk.js"
+  },
+  {
+    "revision": "1ce95ce844ed052cf8c4",
+    "url": "/static/js/runtime-main.16944d45.js"
+  }
+]);
